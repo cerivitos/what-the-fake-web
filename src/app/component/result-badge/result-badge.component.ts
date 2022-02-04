2c2a9121b6bdc218c541146d8eda4945fa651eec
CHANWAIKIT\ceriv <cerivitos@gmail.com>
2022-02-04 15:02:03 +0800

    Add selection border

--- src/app/component/result-badge/result-badge.component.ts
+++ src/app/component/result-badge/result-badge.component.ts
@@ -31,9 +31,13 @@ export class ResultBadgeComponent implements OnInit {
 
   updateStyleClass(selectedRound: number): void {
     if (this.index === selectedRound) {
-      this.styleClass = this.styleClass + ' scale-[0.85]';
+      this.styleClass =
+        this.styleClass + ' scale-[0.85] ring-4 ring-inset ring-white/50';
     } else {
-      this.styleClass = this.styleClass.replace(' scale-[0.85]', '');
+      this.styleClass = this.styleClass.replace(
+        ' scale-[0.85] ring-4 ring-inset ring-white/50',
+        ''
+      );
     }
   }
 }
