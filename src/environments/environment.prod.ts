be2b0ef4a39dec337668efd92c980239883b57f6
CHANWAIKIT\ceriv <cerivitos@gmail.com>
2022-02-04 23:04:56 +0800

    Include firebase in prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBSgcUdG9qfkQ-jLtYl_Hfecnj4OPZROP4',
+    authDomain: 'what-the-fake-web.firebaseapp.com',
+    projectId: 'what-the-fake-web',
+    storageBucket: 'what-the-fake-web.appspot.com',
+    messagingSenderId: '175245071587',
+    appId: '1:175245071587:web:0189272a724cc67bb021d6',
+    measurementId: 'G-VF9W2X6GLM',
+  },
 };
