ee6b68cd319c3c7b22fd43bf1c364b0dbb0f0087
CHANWAIKIT\ceriv <cerivitos@gmail.com>
2022-02-06 00:32:31 +0800

    Test view prod envs

--- api/query-env.ts
+++ api/query-env.ts
@@ -1,10 +1,11 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
+import { environment } from '../src/environments/environment';
 require('dotenv').config();
 
 export default (req: VercelRequest, res: VercelResponse) => {
-  if (process.env.VERCEL_GITHUB_COMMIT_SHA) {
+  if (environment.production && process.env.VERCEL_GITHUB_COMMIT_SHA) {
     res.status(200).send(process.env.VERCEL_GITHUB_COMMIT_SHA?.slice(0, 7));
   } else {
-    res.status(404).send('Not found');
+    res.status(404).send(process.env);
   }
 };
