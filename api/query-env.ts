d2edd2aed4cc21e63a67f85fe51fa22b282f1f3e
CHANWAIKIT\ceriv <cerivitos@gmail.com>
2022-02-06 01:01:39 +0800

    Fix env var query

--- api/query-env.ts
+++ api/query-env.ts
@@ -5,11 +5,7 @@ const dotenv = require('dotenv');
 export default (req: VercelRequest, res: VercelResponse) => {
   dotenv.config();
 
-  console.log(process.env);
-  console.log(environment.production);
-  console.log(process.env.VERCEL_GIT_COMMIT_SHA);
-
-  if (environment.production && process.env.VERCEL_GIT_COMMIT_SHA) {
+  if (process.env.VERCEL_GIT_COMMIT_SHA) {
     res.status(200).send(process.env.VERCEL_GIT_COMMIT_SHA.slice(0, 7));
   } else {
     res.status(404).send('Not found');
