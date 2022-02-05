c23a8da28f72a89f563f0dc915555af9b4e5461a
CHANWAIKIT\ceriv <cerivitos@gmail.com>
2022-02-06 00:53:14 +0800

    Fix field name

--- api/query-env.ts
+++ api/query-env.ts
@@ -1,11 +1,13 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { environment } from '../src/environments/environment';
-require('dotenv').config();
+const dotenv = require('dotenv');
 
 export default (req: VercelRequest, res: VercelResponse) => {
-  if (environment.production && process.env.VERCEL_GITHUB_COMMIT_SHA) {
-    res.status(200).send(process.env.VERCEL_GITHUB_COMMIT_SHA?.slice(0, 7));
+  dotenv.config();
+
+  if (environment.production && process.env.VERCEL_GIT_COMMIT_SHA) {
+    res.status(200).send(process.env.VERCEL_GIT_COMMIT_SHA.slice(0, 7));
   } else {
-    res.status(404).send(process.env);
+    res.status(404).send('Not found');
   }
 };
