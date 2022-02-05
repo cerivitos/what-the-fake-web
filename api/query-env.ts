6194f3677a1785f7800fdf2b1500b13a8f360e1e
CHANWAIKIT\ceriv <cerivitos@gmail.com>
2022-02-05 23:57:37 +0800

    Only return commit hash substring

--- api/query-env.ts
+++ api/query-env.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default (req: VercelRequest, res: VercelResponse) => {
-  res.status(200).json(process.env);
+  res.status(200).json(process.env.VERCEL_GITHUB_COMMIT_SHA?.slice(0, 7));
 };
